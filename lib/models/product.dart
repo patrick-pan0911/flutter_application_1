1a6244a1b784ef6abe9245d66e86f78c0b3883f2
patrick-pan0911 <patrickpym@163.com>
2026-05-14 22:52:50 +0800

    Add product model
    - Added Product model for shopping cart items

---
+++ lib/models/product.dart
@@ -0,0 +1,11 @@
+//task 4
+class Product {
+  final String id;
+  final String name;
+
+  const Product({
+    //must be required to create a product
+    required this.id,
+    required this.name,
+  });
+}
