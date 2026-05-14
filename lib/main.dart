62a03880f3df887a374debec0a829efde35afc84
patrick-pan0911 <patrickpym@163.com>
2026-05-15 03:43:56 +0800

    Set up GetX app routing
    - Replaced MaterialApp with GetMaterialApp
    - Added named routes for product list and cart pages
    - Added initial route configuration
    - Added placeholder product list and cart pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,41 @@
+//task 4
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'pages/cart_page.dart';
+import 'pages/product_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      initialRoute: '/products',
+
+      getPages: [
+        GetPage(
+          name: '/products',
+          page: () => const ProductListPage(),
+        ),
+
+        GetPage(
+          name: '/cart',
+          page: () => const CartPage(),
+        ),
+      ],
+    );
+  }
+}
+
 //task 3
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/repositories/http_post_repository.dart';
 import 'package:flutter_application_1/widgets/posts_page.dart';
@@ -19,6 +56,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
 
 //task2
 /*
---
+++ lib/pages/product_list_page.dart
@@ -0,0 +1,15 @@
+//task 4
+import 'package:flutter/material.dart';
+
+class ProductListPage extends StatelessWidget {
+  const ProductListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Product List Page'),
+      ),
+    );
+  }
+}
