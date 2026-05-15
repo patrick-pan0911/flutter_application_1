ab0ee14e1158f128eccf6088d4da5a7b8e844f4f
patrick-pan0911 <patrickpym@163.com>
2026-05-16 00:57:22 +0800

    Add product list page UI
    - Added product list page layout
    - Added hardcoded product list
    - Added Add to Cart buttons for each product
    - Added scrollable product list using ListView.builder

--- lib/pages/product_list_page.dart
+++ lib/pages/product_list_page.dart
@@ -1,14 +1,45 @@
-//task 4
+// task 4
+// Display a list of products and allow users to add them to the shopping cart
 import 'package:flutter/material.dart';
+//import 'package:get/get.dart';
+
+//import 'package:flutter_application_1/controllers/cart_controller.dart';
+//import 'package:flutter_application_1/models/product.dart';
 
 class ProductListPage extends StatelessWidget {
   const ProductListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Product List Page'),
+    final products = [
+      'Apple',
+      'Banana',
+      'Orange',
+      'Grapes',
+      'Mango',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Products'),
+      ),
+
+      body: ListView.builder(
+        itemCount: products.length,
+
+        itemBuilder: (context, index) {
+          final productName = products[index];
+
+          return ListTile(
+            title: Text(productName),
+
+            trailing: ElevatedButton(
+              onPressed: () {},
+
+              child: const Text('Add to Cart'),
+            ),
+          );
+        },
       ),
     );
   }
