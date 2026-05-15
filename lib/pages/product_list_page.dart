c722c5cfc0c79ce3a6f7276afbe9d1fe98b1bd2b
patrick-pan0911 <patrickpym@163.com>
2026-05-16 00:58:52 +0800

    Add cart item actions
    - Connected product list page to CartController
    - Added Get.find to access shared cart state
    - Added addItem actions for Add to Cart buttons
    - Replaced hardcoded strings with Product objects

--- lib/pages/product_list_page.dart
+++ lib/pages/product_list_page.dart
@@ -1,22 +1,24 @@
 // task 4
 // Display a list of products and allow users to add them to the shopping cart
 import 'package:flutter/material.dart';
-//import 'package:get/get.dart';
+import 'package:get/get.dart';
 
-//import 'package:flutter_application_1/controllers/cart_controller.dart';
-//import 'package:flutter_application_1/models/product.dart';
+import 'package:flutter_application_1/controllers/cart_controller.dart';
+import 'package:flutter_application_1/models/product.dart';
 
 class ProductListPage extends StatelessWidget {
   const ProductListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cart = Get.find<CartController>();
+
     final products = [
-      'Apple',
-      'Banana',
-      'Orange',
-      'Grapes',
-      'Mango',
+      const Product(id: '1', name: 'Apple'),
+      const Product(id: '2', name: 'Banana'),
+      const Product(id: '3', name: 'Orange'),
+      const Product(id: '4', name: 'Grapes'),
+      const Product(id: '5', name: 'Mango'),
     ];
 
     return Scaffold(
@@ -28,13 +30,15 @@ class ProductListPage extends StatelessWidget {
         itemCount: products.length,
 
         itemBuilder: (context, index) {
-          final productName = products[index];
+          final product = products[index];
 
           return ListTile(
-            title: Text(productName),
+            title: Text(product.name),
 
             trailing: ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                cart.addItem(product);
+              },
 
               child: const Text('Add to Cart'),
             ),
